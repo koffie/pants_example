a48515a5b4981dd95ace6e4a2e05c1d850e20f36
Maarten Derickx <maarten.derickx@navara.nl>
2022-05-27 18:41:06 +0200

    Move dependencies from test to distribution

--- src/python/my-subproject/my_subproject/my_script.py
+++ src/python/my-subproject/my_subproject/my_script.py
@@ -1,4 +1,10 @@
 from my_subproject import __version__
 
-if __name__ == "__main__":
+
+def main():
     print(f"running version: {__version__}")
+
+
+if __name__ == "__main__":
+    main()
+    print("from main")
