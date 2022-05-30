b639754fbc01d4dad985a6a26e6eeaf69e5d95ad
Maarten Derickx <maarten.derickx@navara.nl>
2022-05-30 13:16:06 +0200

    add clearer installed test

--- src/python/my-subproject/my_subproject/__init__.py
+++ src/python/my-subproject/my_subproject/__init__.py
@@ -2,9 +2,8 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
-__version__ = version(__name__)
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
-    pass
+    __version__ = None
--- src/python/my-subproject/tests/test_my_subproject.py
+++ src/python/my-subproject/tests/test_my_subproject.py
@@ -1,5 +1,11 @@
+import importlib.util
+
 from my_subproject import __version__
 
+def test_installed() -> None:
+    spec = importlib.util.find_spec("my_submodule")
+    assert spec is not None
+    # assert "site-packages" in spec.origin
 
 def test_version() -> None:
     assert __version__ == "0.1.0"
